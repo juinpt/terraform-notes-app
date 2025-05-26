f5aa2380ad563db0d097da260770d24c593f6ea3
Joao Afonso <joao@afonso.jp>
2025-05-27 03:45:41 +0900

    fixed certificate in alb

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "aws_lb_listener" "front_end" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.cert.arn
+  certificate_arn   = aws_acm_certificate_validation.cert.certificate_arn
 
   default_action {
     type             = "forward"
