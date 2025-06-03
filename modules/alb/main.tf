d0e192ba699ca0831bfd598312bb81b721e51873
Joao <joao@afonso.jp>
2025-06-03 11:07:55 +0900

    typo

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -12,7 +12,7 @@ resource "aws_lb_target_group" "app" {
   port        = 8080
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
-  target_type = ip
+  target_type = "ip"
 
   health_check {
     path                = "/"
